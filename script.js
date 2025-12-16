238f288fbe80a38d18f927c301a1d5e5f37979e3
Soulful50s <soulfulfifties@gmail.com>
2025-12-15 22:52:49 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Este es nuestro archivo JavaScript
+console.log('¡La página está funcionando!');
+
+// Un saludo simple en la consola
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Bienvenido a tu sitio web, Soulful50s! 🐝');
+});
